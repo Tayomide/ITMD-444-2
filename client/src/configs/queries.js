3835fb73f655a64520bef681275673d9323657d9
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-03-28 20:58:42 -0500

    Define queries

---
+++ client/src/configs/queries.js
@@ -0,0 +1,54 @@
+import { gql } from '@apollo/client'
+
+// Fetch all posts with titles and authors
+const GetPosts = gql`
+  query GetPosts {
+    feed {
+      id
+      title
+      viewCount
+      updatedAt
+      author {
+        id
+        name
+      }
+    }
+  }
+`
+
+// Fetch a single post by ID
+const GetPostById = gql`
+  query GetPostById($id: String!) {
+    postById(id: $id) {
+      id
+      title
+      content
+      author {
+        id
+        name
+      }
+    }
+  }
+`
+
+// Fetch posts by a user
+const GetPostsByUser = gql`
+  query GetPostsByUser($userId: String!) {
+    draftsByUser(userUniqueInput: { id: $userId }) {
+      id
+      title
+      viewCount
+      updatedAt
+      author{
+        name
+        email
+      }
+    }
+  }
+`
+
+export {
+  GetPosts,
+  GetPostById,
+  GetPostsByUser
+}
