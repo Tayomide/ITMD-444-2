441a1cf4ce0aa2741f69e2c63afd267fe8339eee
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-03-28 22:12:44 -0500

    Update UI

--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,15 +1,22 @@
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GetPostById } from '../configs/queries'; // Assume the query is defined externally
+import { useParams, useNavigate } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
+import { GetPostById } from '../configs/queries';
+import { DeletePostById } from '../configs/mutations';
+
 import styled from 'styled-components';
 
 const PostDetail = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const postId = params.postId;
   const { loading, error, data } = useQuery(GetPostById, {
     variables: { id: postId },
   });
-
+  const [deletePost] = useMutation(DeletePostById, {
+    variables: { id: postId },
+    onCompleted: () => navigate('/posts'),
+  })
+  
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error {":("}</p>;
   const { title, content, author } = data.postById;
@@ -19,6 +26,8 @@ const PostDetail = () => {
       <h1>{title}</h1>
       <p>by <a href={`/user/${author.id}`}>{author.name}</a></p>
       <p>{content}</p>
+      <button onClick={deletePost}>Delete</button>
+      <a href='/posts' className='small'>Back to posts</a>
     </Container>
   );
 };
@@ -34,6 +43,29 @@ const Container = styled.div`
     color: #9e9eff;
     &:hover {
       text-decoration: underline;
+    }
+    &.small{
+      font-size: .9em;
+      display: block;
+      margin-top: 1em;
+    }
+  }
+  div{
+    margin-top: 1em;
+  }
+  button{
+    font-size: 1em;
+    border: 1px solid rgba(255, 255, 255, 0.87);
+    padding: .5em;
+    border-radius: 0.5em;
+    color: #ff9e9e;
+    background-color: transparent;
+    cursor: pointer;
+    display: block;
+    margin-top: 1em;
+    &:hover{
+      background-color: #ff9e9e;
+      color: black;
     }
   }
 `
